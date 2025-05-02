4e6c141b97c91eb936f26618ea31d4aa8a7ae2c8
julinza <julinza@example.com>
2025-05-02 14:38:33 -0300

    Agrego el menu de opciones de operaciones con matrices, y también agrego la opción ya funcionando correctamente de la suma y resta de matrices que deben ser de igual dimensión si o si.

--- calculadora/calculadora.cpp
+++ calculadora/calculadora.cpp
@@ -7,8 +7,8 @@ int ingresarOpcionMenu();
 //Opcion 1.
 int ingresarOpciones1();
 void menuOpciones1(int *opc);
-//Operaciones
 void ingresarNumeros(float *num, float *num1, int *opc, char *letra);
+//Operaciones
 void suma(float num, float num1);
 void resta(float num, float num1);
 void multiplicacion(float num, float num1);
@@ -16,10 +16,18 @@ void division(float num, float num1);
 void potencia(float base, float exponente);
 void raiz(float indice, float radicando);
 
+//Opcion 3
+int ingresarOpciones3();
+void menuOpciones3(int opc);
+void ingresarMatrizAyB(int *F, int *C, int matrizA[10][10], int matrizB[10][10]);
+//Operaciones
+void matrizSuma(int F, int C, int A[10][10], int B[10][10]);
+void matrizResta(int F, int C, int A[10][10], int B[10][10]);
+
 
 
 int main(){
-	int opcionMenu, opcion1;
+	int opcionMenu, opcion1, opcion3;
 	
 	printf("Calculadora:");
 	
@@ -42,7 +50,8 @@ int main(){
 			break;
 			
 			case 3:
-				printf("\n\nHola mundo.\n\n");
+				opcion3 = ingresarOpciones3();
+				menuOpciones3(opcion3);
 			break;
 			
 			case 4:
@@ -222,4 +231,142 @@ void raiz(float radical, float indice){
 	printf("\n\nRaiz: \n");
 	float resultado = pow(radical, (1 / indice));
 	printf("\nLa raiz %f de %f es igual a: %f", radical, indice, resultado);
+}
+
+int ingresarOpciones3(){
+	int opcMenu3;
+	
+	printf("\n\n\tIngrese 1. Si quiere realizar operaciones como Suma y Resta de Matrices.");
+	printf("\n\n\tIngrese 2. Si quiere realizar Multiplicacion de un Escalar por una Matriz.");
+	printf("\n\n\tIngrese 3. Si quiere realizar Multiplicacion de Matrices.");
+	printf("\n\n\tIngrese 4. Si quiere realizar el Determinante de una Matriz.");
+	printf("\n\n\tIngrese 5. Si quiere realizar la Inversa de una Matriz.");
+	printf("\n\n\tIngrese 6. Si quiere realizar una Division entre dos Matrices.");	
+	printf("\n\n\n\tIngrese una opcion a elegir: > ");
+	scanf("%d", &opcMenu3);
+	while(opcMenu3 < 1 || opcMenu3 > 6){
+		printf("\n\tOpcion no valida.\n\n\tPor favor ingrese de nuevo la opcion correspondiente: > ");
+		scanf("%d", &opcMenu3);
+	}	
+	return opcMenu3;
+}
+
+void menuOpciones3(int opc){
+	char opcLetra;
+	int filas, columnas, matrizA[10][10], matrizB[10][10];
+	
+	switch(opc){
+		
+		case 1:
+			printf("\n\n\tIngrese S. Para realizar una Suma entre Matrices.\n");
+			printf("\tIngrese R. Para realizar una Resta entre Matrices.\n\n");
+			printf("\tIngrese una opcion: > ");
+			scanf(" %c", &opcLetra);
+			opcLetra = toupper(opcLetra);
+			
+			while(opcLetra != 'S' && opcLetra != 'R'){
+				printf("\n\tOpcion no valida.\n\n\tPor favor ingrese de nuevo la opcion correspondiente: > ");
+				scanf(" %c", &opcLetra);
+			}
+			
+			ingresarMatrizAyB(&filas, &columnas, matrizA, matrizB);
+			
+			switch(opcLetra){
+				case 'S':
+					matrizSuma(filas, columnas, matrizA, matrizB);	
+				break;
+				
+				case 'R':
+					matrizResta(filas, columnas, matrizA, matrizB);	
+				break;
+			}
+			
+		break;
+		case 2:
+		break;
+		case 3:
+		break;
+		case 4:
+		break;
+		case 5:
+		break;
+		case 6:
+		break;
+	}
+}
+
+void ingresarMatrizAyB(int *F, int *C, int matrizA[10][10], int matrizB[10][10]){
+	int i, j;
+	
+	printf("\n\n\tIngrese la cantidad de filas: ");
+	scanf("%d", F);
+	while(*F < 1 || *F > 10){
+		printf("Cantidad no valida, considere entre los valores del 1 al 10: ");
+		scanf("%d", F);
+	}
+	
+	printf("\tIngrese la cantidad de columnas: ");
+	scanf("%d", C);
+	while(*C < 1 || *C > 10){
+		printf("Cantidad no valida, considere entre los valores del 1 al 10: ");
+		scanf("%d", C);
+	}
+	
+	printf("\n\n\tIngrese los valores de la matriz A:\n");
+	
+	for(i=0; i < *F; i++){
+		for(j=0; j < *C; j++){
+			printf("\t\tA[%d][%d]: > ", i, j);
+			scanf("%d", &matrizA[i][j]);
+		}
+	}
+	
+ 	printf("\n\n\tIngrese los valores de la matriz B:\n");
+ 	
+ 	for(i=0; i < *F; i++){
+ 		for(j=0; j < *C; j++){
+ 			printf("\t\tB[%d][%d]: > ", i, j);
+ 			scanf("%d", &matrizB[i][j]);
+		 }
+	 }
+		
+}
+
+void matrizSuma(int F, int C, int A[10][10], int B[10][10]){
+	int i, j, resultado[10][10];
+	
+	for(i=0; i < F; i++){
+		for(j=0; j < C; j++){
+			resultado[i][j] = A[i][j] + B[i][j];
+		}
+	}
+	
+	printf("\n\nLa Matriz Resultado de la Suma entre esas dos matrices es igual a:\n\n");
+	
+	for(i=0; i < F; i++){
+		for(j=0; j < C; j++){
+			printf("|%d|", resultado[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void matrizResta(int F, int C, int A[10][10], int B[10][10]){
+	int i, j, resultado[10][10];
+	
+	for(i=0; i < F; i++){
+		for(j=0; j < C; j++){
+			resultado[i][j] = A[i][j] - B[i][j];
+		}
+	}
+	
+	printf("\n\nLa Matriz Resultado de la Resta entre esas dos matrices es igual a:\n\n");
+	
+	for(i=0; i < F; i++){
+		for(j=0; j < C; j++){
+			printf("|%d|", resultado[i][j]);
+		}
+		printf("\n");
+	}
+	
 }
